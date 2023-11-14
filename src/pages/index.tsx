b4ab63a8240ae0249a8d33f283bde6b2cbc181f8
Vinicius Pretto <vini.prettodasilva@gmail.com>
2023-11-14 13:16:57 -0300

    adding users endpoint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
+import { User } from "@/types/user";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+type UsersResponse = {
+  users: User[];
+};
+
 export default function Home() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  console.log("users", users);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((response) => response.json())
+      .then((usersReponse: UsersResponse) => {
+        setUsers(usersReponse.users);
+      });
+  }, []);
+
   return (
     <>
       <Head>
@@ -13,7 +31,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={inter.className}>
-        <h1>MSW Sample</h1>
+        <h1>Users</h1>
+        <table border={1}>
+          <thead>
+            <tr>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Email name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </main>
     </>
   );
