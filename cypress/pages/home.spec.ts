9a6927d144e8fcb3dd45e12da1a6255fde3bb48b
Vinicius Pretto <vini.prettodasilva@gmail.com>
2023-11-14 13:40:52 -0300

    adding cypress interceptor

--- cypress/pages/home.spec.ts
+++ cypress/pages/home.spec.ts
@@ -1,5 +1,12 @@
+import usersResponse from "../../src/fixtures/users.json";
+
 describe("Home", () => {
   it("should render the users", () => {
+    cy.intercept("GET", "**/api/users", {
+      statusCode: 200,
+      body: usersResponse,
+    });
+
     cy.visit("/");
 
     cy.contains("Ronald").should("be.visible");
